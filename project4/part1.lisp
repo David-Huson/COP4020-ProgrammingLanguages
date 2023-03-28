0f8b37e2d4477f37cdbfd3f54f43841c4f92ac7d
David Huson <dhuson@davidhuson.dev>
2023-03-28 13:46:34 -0500

    part 1 mostly complete

--- project4/part1.lisp
+++ project4/part1.lisp
@@ -1,17 +1,24 @@
+
 (setq fsa '((0 0 x) (0 1 y) (1 2 x) (2 2 x) (2 3 y) (3 3 x) (3 4 z) (4 4 x) (4 2 y) (4 1 a)))
 (setq input '(x x x x x y x x x y x x x z x x x a))
 (setq acceptStates '(1 3))
 (setq alphabet '(x y z a))
 (setq initialState '(0))
-(setq state (car initialState))
-(dolist (c input)
-  (setq input (cdr input)) 
-  (dolist (trans fsa)
+
+(defun testFSA (inputStringAsList fsaDescription acceptingStatesList alphabetList initialStateAsList)
+(setq state (car initialStateAsList))
+(dolist (c inputStringAsList)
+  ;; (setq input (cdr input)) 
+  (dolist (trans fsaDescription)
     (setq tState (car trans))
     (setq token (caddr trans))
     (setq nState (cadr trans))
-    (format t "tState: ~a, token: ~a, nState: ~a, state: ~a, c: ~a\n" tState token nState state c)
-    ;; (cond (eq NIL (member c alphabet))((print "Not accepted") (return NIL)))
+    ;; (format t "tState: ~a, token: ~a, nState: ~a, state: ~a, c: ~a\n" tState token nState state c)
+    (unless ;; if c is not in the alphabet, print "Not Accepted" and exit exicution
+      (member c alphabetList)
+      (setq state -1) (return NIL) 
+      
+    )
     (cond 
       ((eq state tState)
       (cond 
@@ -19,10 +26,12 @@
         (setq state nState)))
       ))
   )  
-  (format t "State is: ~a after ~a\n" state c)
-  (cond ((null input)(return state)))
+  ;; (format t "State is: ~a after ~a\n" state c)
+  (cond ((null inputStringAsList)(return state)))
 )
-(cond ((member state acceptStates)(print "The string is accepted in this language!")))
-
-
-  
+(if (member state acceptingStatesList)
+    (format nil "The string is accepted in this language!")
+  ;else
+    (format nil "Not Accepted!")
+)   
+)
